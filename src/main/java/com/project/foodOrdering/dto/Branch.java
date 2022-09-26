c37cdcf56840c5618770e7443aec0deada49dad7
Umaang <umaang@clarivate.com>
2022-09-26 13:42:27 +0530

    User Class Code is Added

--- src/main/java/com/project/foodOrdering/dto/Branch.java
+++ src/main/java/com/project/foodOrdering/dto/Branch.java
@@ -1,5 +1,68 @@
 package com.project.foodOrdering.dto;
 
-public class Branch {
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Branch {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String BranchName;
+	private String BranchAddress;
+	private long BranchPhone;
+	private String BranchEmail;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private User user;
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBranchName() {
+		return BranchName;
+	}
+
+	public void setBranchName(String branchName) {
+		BranchName = branchName;
+	}
+
+	public String getBranchAddress() {
+		return BranchAddress;
+	}
+
+	public void setBranchAddress(String branchAddress) {
+		BranchAddress = branchAddress;
+	}
+
+	public long getBranchPhone() {
+		return BranchPhone;
+	}
+
+	public void setBranchPhone(long branchPhone) {
+		BranchPhone = branchPhone;
+	}
+
+	public String getBranchEmail() {
+		return BranchEmail;
+	}
+
+	public void setBranchEmail(String branchEmail) {
+		BranchEmail = branchEmail;
+	}
 }
